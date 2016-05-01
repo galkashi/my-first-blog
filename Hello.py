0daa8aab706c2ba6cba730fbf673b7dc93448c49
Gal <gal.k33@gmail.com>
2016-05-01 12:28:57 +0300

    My Django Girls app, first commit

---
+++ Hello.py
@@ -0,0 +1,14 @@
+def politeHello(name):
+	print("Hello "+ name)
+
+
+print("Hello!")
+name = input("What is your name?")
+#name = "gal"
+politeHello(name)
+
+dict = {1:"hi",2:"gal"}
+print(dict[1])
+print(dict.values())
+
+
